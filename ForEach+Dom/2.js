f66f3157f4827f31e6248fac4be69608cfbf90d9
黃昱銘 <dive880217@gmail.com>
2022-12-06 14:34:50 +0800

    forEach with if、obj and array data

---
+++ ForEach+Dom/2.js
@@ -0,0 +1,43 @@
+const list = document.querySelector('.list')
+// list.innerHTML = '<li>www</li>' // test success
+
+let data = [
+    {
+        sex : 'male',
+        name : 'Tom',
+        age : 12
+     },
+     {
+        sex : 'female',
+        name : 'May',
+        age : 16
+     },
+     {
+        sex : 'male',
+        name : 'Jom',
+        age : 22
+     },
+     {
+        sex : 'female',
+        name : 'BlackTea',
+        age : 43
+     },
+     {
+        sex : 'female',
+        name : 'Ash',
+        age : 24
+     }
+]
+
+let set = {
+    male : 0,
+    female: 0,
+} 
+data.forEach((item)=>{
+   if(item.sex === 'female'  ){
+    set.female+=1 
+   } else if(item.sex === 'male'){
+    set.male+=1 
+   }
+})
+console.log(set)
